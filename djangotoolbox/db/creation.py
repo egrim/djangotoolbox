98a870aa70df76b768707aab0b8dc291846c63b3
wrwrwr <git@wr.waw.pl>
2012-01-19 18:05:22 +0100

    Use an abstract key type for storing primary and foreign keys.

--- djangotoolbox/db/creation.py
+++ djangotoolbox/db/creation.py
@@ -1,15 +1,34 @@
 from django.db.backends.creation import BaseDatabaseCreation
 
+
 class NonrelDatabaseCreation(BaseDatabaseCreation):
+    # This dictionary maps Field objects to back-end types that should
+    # be used to store their values. Type strings can contain format
+    # strings; they'll be interpolated against the values of
+    # Field.__dict__ before being output. If a type is set to
+    # None, it won't be included in the output.
     data_types = {
-        'AutoField':         'integer',
+
+        # NoSQL databases usually have specific concepts of keys. For
+        # example, GAE has the db.Key class, MongoDB driver only allows
+        # ObjectIds, Redis uses strings, while Cassandra supports
+        # different types (including binary data).
+        'AutoField':         'key',
+        'ForeignKey':        'key',
+        'OneToOneField':     'key',
+        'RelatedAutoField':  'key',
+
+        # Standard fields types, more or less suitable for databases
+        # (or its client / driver) being able to directly store or
+        # process Python objects, but having separate types for storing
+        # short and long strings.
         'BigIntegerField':   'long',
         'BooleanField':      'bool',
         'CharField':         'text',
         'CommaSeparatedIntegerField': 'text',
         'DateField':         'date',
         'DateTimeField':     'datetime',
-        'DecimalField':      'decimal:%(max_digits)s,%(decimal_places)s',
+        'DecimalField':      'decimal',
         'EmailField':        'text',
         'FileField':         'text',
         'FilePathField':     'text',
@@ -18,7 +37,6 @@ class NonrelDatabaseCreation(BaseDatabaseCreation):
         'IntegerField':      'integer',
         'IPAddressField':    'text',
         'NullBooleanField':  'bool',
-        'OneToOneField':     'integer',
         'PositiveIntegerField': 'integer',
         'PositiveSmallIntegerField': 'integer',
         'SlugField':         'text',
@@ -28,12 +46,27 @@ class NonrelDatabaseCreation(BaseDatabaseCreation):
         'URLField':          'text',
         'XMLField':          'longtext',
 
+         # Mappings for fields provided by nonrel.
         'BlobField':         'blob',
         'RawField':          'raw',
     }
 
-    def sql_create_model(self, *args, **kwargs):
+    def db_type(self, field):
+        """
+        Use "key" type for primary key fields independent of the field
+        class, for other fields use the original Django logic.
+        """
+        if field.primary_key:
+            return 'key'
+        return super(NonrelDatabaseCreation, self).db_type(field)
+
+    def sql_create_model(self, model, style, known_models=set()):
+        """
+        Most NoSQL databases are schema-less, no data definitions are
+        needed.
+        """
         return [], {}
 
-    def sql_indexes_for_model(self, *args, **kwargs):
+    def sql_indexes_for_model(self, model, style):
+        """Creates all indexes needed for local fields of a model."""
         return []
